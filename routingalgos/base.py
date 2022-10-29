369ccf4bc5b04ebcba111c01847db25dc94d6e1b
Ellariel <ell.science@mail.ru>
2022-10-29 15:16:09 +0200

    Add files via upload

--- routingalgos/base.py
+++ routingalgos/base.py
@@ -34,23 +34,23 @@ class Routing:
                 continue
             visited.add(curr)
             for [v, curr] in G.in_edges(curr):
-                if payment_source and v == source and G.edges[v, curr]["Balance"] >= amount[curr]:
+                if payment_source and v == source and G.edges[v, curr]["balance_sat"] >= amount[curr]:
                     cost = dist[curr] + self.cost_function_no_fees(G, amount[curr], curr, v)
                     if cost < dist[v]:
                         dist[v] = cost
                         paths[v] = [v] + paths[curr]
-                        delay[v] = G.edges[v, curr]["Delay"] + delay[curr]
+                        delay[v] = G.edges[v, curr]["delay"] + delay[curr]
                         amount[v] = amount[curr]
                         pq.put((dist[v], v))
-                if(G.edges[v, curr]["Balance"] + G.edges[curr, v]["Balance"] >= amount[curr]) and v not in visited:
+                if(G.edges[v, curr]["balance_sat"] + G.edges[curr, v]["balance_sat"] >= amount[curr]) and v not in visited:
                     if (v != source or not payment_source):
                         cost = dist[curr] + self.cost_function(G, amount[curr], curr, v)
                         if cost < dist[v]:
                             dist[v] = cost
                             paths[v] = [v] + paths[curr]
-                            delay[v] = G.edges[v, curr]["Delay"] + delay[curr]
-                            amount[v] = amount[curr] + G.edges[v, curr]["BaseFee"] + \
-                                amount[curr]*G.edges[v, curr]["FeeRate"]
+                            delay[v] = G.edges[v, curr]["delay"] + delay[curr]
+                            amount[v] = amount[curr] + G.edges[v, curr]["fee_base_sat"] + \
+                                amount[curr]*G.edges[v, curr]["fee_rate_sat"]
                             pq.put((dist[v], v))
         return [], -1, -1, -1
 
